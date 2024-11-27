e9b992904b0b2a7cd72411b642e720c20c3a343b
jaserino <jaserino1@gmail.com>
2024-11-27 09:39:44 -0500

    manipulating our response data

--- main.py
+++ main.py
@@ -18,19 +18,41 @@ timestamp_str = timestamp.strftime("%m/%d/%Y, %H:%M:%S")
 #hashing our calls made with our keys
 hash_value = hashlib.md5((timestamp_str+private_key+public_key).encode()).hexdigest()
 
+
+#function to make our API call
+
+
 #declaring api url
 base_api = "http://gateway.marvel.com/v1/public/characters"
+
+
 
 #creating params for api request
 params = {
     "apikey": public_key,
     "ts": timestamp_str,
     "hash": hash_value,
-    "limit": 50
+    "limit": 100
 }
 
 # making our api request
-response = requests.get(base_api, params=params)
+data = get_request(base_api, params=params)
+# manipulating our response data
+characters = data['data']['results']
+for char in characters:
+    name = char['name']
+    #getting rid of the apostrophe
+    name = name.replace("'",'')
+    description = char['description']
+    description = description.replace("'",'')
+    resourceURI = char['resourceURI']
+    thumbnail = char['thumbnail']['path']+'.'+['thumbnail']['extension']
+    comics = char['comics']['available']
+
+    results_str = f"{name}{description}{resourceURI}{thumbnail}{comics}"
+
+    print(results_str)
+
 # putting our response in readable format
-data = response.json()
-print(data)
+# data = response.json()
+# print(data)
